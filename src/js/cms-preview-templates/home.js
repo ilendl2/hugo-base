9d00c492aca33772cc5bcfa059d2e19a409cc4fd
Lendl Smith <lendl.smith@gmail.com>
2019-09-03 12:25:56 -0400

    no message

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -16,49 +16,21 @@ export default class PostPreview extends React.Component {
     return <div>
         <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
 
-        <div id="bg-grey" className="pad">
-          <div className="grid-container pad">
-            <div className="grid-x grid-padding-x grid-padding-y align-center">
-              <div className="medium-8 cell">
-                <center>
-                  <h2><strong>{entry.getIn(["data", "intro", "heading"])}</strong></h2>
-                  <p>{entry.getIn(["data", "intro", "text"])}</p>
-                </center>
-              </div>
-            </div>
-          </div>
+        <div>
+          <h2>{entry.getIn(["data", "intro", "heading"])}</h2>
+          <p>{entry.getIn(["data", "intro", "text"])}</p>
         </div>
 
-        <div id="bg-off-white" className="pad">
-          <div className="grid-container pad">
-            <div className="grid-x grid-padding-x align-center">
-              <div className="medium-10 cell">
-                <div className="grid-x grid-padding-x align-middle">
-                  <div className="medium-5 cell">
-                    <h2><strong>{entry.getIn(["data", "blurb", "heading"])}</strong></h2>
-                  </div>
-                  <div className="medium-7 cell">
-                    <p>{entry.getIn(["data", "blurb", "text"])}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div>
+          <h2>{entry.getIn(["data", "blurb", "heading"])}</h2>
+          <p>{entry.getIn(["data", "blurb", "text"])}</p>
         </div>
 
-        <div id="bg-grey">
-          <div id="media-block" className="grid-container pad">
-        		<div className="grid-x grid-padding-x grid-padding-y align-middle">
-        			<div className="medium-6 cell">
-        				<img src="img/home-about-section.jpg" alt="">
-        			</div>
-        			<div className="medium-6 cell">
-        				<h2><strong>{entry.getIn(["data", "block", "heading"])}</strong></h2>
-        				<p>{entry.getIn(["data", "block", "text"])}</p>
-        				<a href="{{.buttonLink}}" className="button">Read more</a>
-        			</div>
-        		</div>
-        	</div>
+        <div>
+          <h2>{entry.getIn(["data", "block", "heading"])}</h2>
+          <p>{entry.getIn(["data", "block", "text"])}</p>
+          <a href="{{.buttonLink}}">Read more</a>
+          <img src="/img/home-about-section.jpg" alt="" width="400" className="mb3"/>
         </div>
     </div>
   }
