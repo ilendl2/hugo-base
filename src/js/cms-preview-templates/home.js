a9fdf80a814718320f3ae97903f52688efb300be
Lendl Smith <lendl.smith@gmail.com>
2019-09-03 12:09:52 -0400

    no message

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -16,6 +16,13 @@ export default class PostPreview extends React.Component {
     return <div>
         <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
 
+        <div className="bg-off-white pv4">
+          <div className="ph3 mw7 center">
+            <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "intro", "heading"])}</h2>
+            <p className="mb4 mw6">{entry.getIn(["data", "intro", "text"])}</p>
+          </div>
+        </div>
+
         <div className="bg-grey-1 pv4">
           <div className="flex-l mhn1-l ph3 center mw7">
             <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}</h2>
@@ -37,8 +44,6 @@ export default class PostPreview extends React.Component {
             </div>
           </div>
         </div>
-
-
     </div>
   }
 }
