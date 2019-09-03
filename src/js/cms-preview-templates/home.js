6349c592ad348d98263c300a6794a7e4c628872b
Lendl Smith <lendl.smith@gmail.com>
2019-09-03 14:36:14 -0400

    no message

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -7,6 +7,7 @@ export default class PostPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
+    let imageUrl = getAsset(entry.getIn(["data", "block", "imageUrl"]));
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
